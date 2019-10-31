9d31015736f882b50c9674a0a83756382f0006fd
永冬 <you@example.com>
2019-10-31 15:59:56 +0800

    ABC

--- test/routes/users.js
+++ test/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource 777');
+  res.send('respond with a resource 777888');
 });
 
 
